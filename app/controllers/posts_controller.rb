a6497e74209c11cc53e4b290e0acb888826a4293
MauricioNC <mauricionc19@gmail.com>
2023-11-10 15:23:23 -0600

    feat: Functionality to create a new post completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to root_path, notice: "Post created successfully"
+    else
+      render :new, notice: "Something went wrong, try again"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :content)
+  end
 end
